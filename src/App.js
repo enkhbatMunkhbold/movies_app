bb5793834cf7f0d4bca3e0cbd361e63de8760965
Enkhbat Munkhbold <emunkhbold10@gmail.com>
2022-07-11 12:47:16 -0700

    Modify home page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
+import Navbar from "./components/navigation/Navbar";
 import Home from "./components/static/Home"
 
 const App = () => {
   return (
     <div className="App">
+      <Navbar />
       <Home />
     </div>
   );
