e6808fabb0ab0aaab4278a88781666a280bce9a4
Enkhbat Munkhbold <emunkhbold10@gmail.com>
2022-07-14 23:32:17 -0700

    Spacing cards

--- src/components/static/MovieList.js
+++ src/components/static/MovieList.js
@@ -1,5 +1,17 @@
 import React from 'react'
-import MovieCard from "./MovieCard"
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
+import MovieCard from "./MovieCard";
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
 
 const MovieList = ({ movies }) => {    
   console.log(movies);
@@ -8,7 +20,22 @@ const MovieList = ({ movies }) => {
   })
 
   return (
-    <div>{movieCards}</div>
+    <Box sx={{ flexGrow: 1 }} style={{ marginTop: "30px"}}>
+      <Grid container spacing={4}>
+        <Grid item xs={3}>
+          <Item>xs=4</Item>
+        </Grid>
+        <Grid item xs={3}>
+          <Item>xs=4</Item>
+        </Grid>
+        <Grid item xs={3}>
+          <Item>xs=4</Item>
+        </Grid>
+        <Grid item xs={3}>
+          <Item>xs=4</Item>
+        </Grid>
+      </Grid>
+    </Box>
   )
 }
 
