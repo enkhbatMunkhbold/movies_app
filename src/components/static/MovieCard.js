7886fdfa3dba0855445067b90cb5cd8e094e7972
Enkhbat Munkhbold <emunkhbold10@gmail.com>
2022-07-15 22:50:33 -0700

    Create demo file to make and try changes

--- src/components/static/MovieCard.js
+++ src/components/static/MovieCard.js
@@ -1,14 +1,46 @@
 import * as React from 'react';
 import Card from '@mui/material/Card';
+import { makeStyles } from '@material-ui/core/styles';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
-import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import clsx from 'clsx';
+import Collapse from '@material-ui/core/Collapse';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    maxWidth: 345,
+  },
+  media: {
+    height: 0,
+    paddingTop: '56.25%', // 16:9
+  },
+  expand: {
+    transform: 'rotate(0deg)',
+    marginLeft: 'auto',
+    transition: theme.transitions.create('transform', {
+      duration: theme.transitions.duration.shortest,
+    }),
+  },
+  expandOpen: {
+    transform: 'rotate(180deg)',
+  }
+}));
 
 const MovieCard = ({ movie }) => {
+
+  const classes = useStyles();
+  const [expanded, setExpanded] = React.useState(false);
+
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
   return (
-    <Card sx={{ maxWidth: 345 }}>
+    <Card className={classes.root}>
       <CardMedia
         component="img"
         height="500"
@@ -19,15 +51,38 @@ const MovieCard = ({ movie }) => {
         <Typography gutterBottom variant="h5" component="div">
           { movie.name }
         </Typography>
-        <Typography variant="body2" color="text.secondary">
-          Lizards are a widespread group of squamate reptiles, with over 6,000
-          species, ranging across all continents except Antarctica
+        <Typography gutterBottom variant="p" component="div">
+          Genre: { movie.genre }
         </Typography>
+        <Typography gutterBottom variant="p" component="div">
+          Year: { movie.year }
+        </Typography>
+        <Typography gutterBottom variant="p" component="div">
+          Rating: { movie.Rating }
+        </Typography>       
       </CardContent>
       <CardActions>
-        <Button size="small">Share</Button>
-        <Button size="small">Learn More</Button>
+        <IconButton aria-label="add to favorites">
+          <FavoriteIcon />
+        </IconButton>       
+        <IconButton
+          className={clsx(classes.expand, {
+            [classes.expandOpen]: expanded,
+          })}
+          onClick={handleExpandClick}
+          aria-expanded={expanded}
+          aria-label="show more"
+        >
+          <ExpandMoreIcon />
+        </IconButton>
       </CardActions>
+      <Collapse in={expanded} timeout="auto" unmountOnExit>
+        <CardContent>          
+          <Typography paragraph>
+           {movie.comment}
+          </Typography>         
+        </CardContent>
+      </Collapse>
     </Card>
   );
 }
