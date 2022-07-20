65ad563e937af8e2b94e0b9f67628e81d2ec5afc
Enkhbat Munkhbold <emunkhbold10@gmail.com>
2022-07-19 17:47:17 -0700

    Current status

--- src/components/static/MovieCard.js
+++ src/components/static/MovieCard.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { useState } from 'react';
+// import { useState } from 'react';
 import Card from '@mui/material/Card';
 import { makeStyles } from '@material-ui/core/styles';
 import CardActions from '@mui/material/CardActions';
